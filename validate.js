f418714bfc09b2d88dd546fd8c5b532b2e7d6137
Jess <hi@jesstechpreneur.com>
2024-05-18 02:16:01 -0500

    Updated Validation Javascript

--- validate.js
+++
@@ -1,7 +0,0 @@
-const email = document.getElementById("#emailAddress");
-const confirm = document.getElementById("#emailConfirm");
-
-
-function checkEmail() {
-  let (email.value == "" && confirm.value !== "");
-}
