4ecb5905d42c15b6374e0c36202a80d114ad46ca
Jess <hi@jesstechpreneur.com>
2024-05-18 01:15:48 -0500

    Added Form Javascript

---
+++ validate.js
@@ -0,0 +1,7 @@
+const email = document.getElementById("#emailAddress");
+const confirm = document.getElementById("#emailConfirm");
+
+
+function checkEmail() {
+  let (email.value == "" && confirm.value !== "");
+}
